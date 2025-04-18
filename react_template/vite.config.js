f9e2d4367ee199a5d183e8ec5cbb33bf78cdb6b9
MGX <admin@mgx.dev>
2025-04-18 08:25:14 -0700

    Fixed Cloudflare timeout errors.

--- react_template/vite.config.js
+++ react_template/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    hmr: {
+      timeout: 10000,
+      protocol: 'ws',
+      host: 'localhost'
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  }
 })
